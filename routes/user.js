c42c70bde5969bff079d64fe0fc2df6d7256afdb
As581 <av0232016@gmail.com>
2024-11-30 17:03:06 +0530

    Create user.js

---
+++ routes/user.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const { userModel,validateUser } = require('../models/user');
+
+
+router.get('/login',(req,res)=>{
+     res.render("user_login");
+});
+router.get('/profile',(req,res)=>{
+     res.send("Profile");
+});
+router.get('/logout',(req,res,next)=>{
+    req.logout(function(err) {
+    if (err) { return next(err); }
+      req.session.destroy((err)=>{
+           if(err) return next(err);
+           res.clearCookie("connect.sid");
+           res.redirect("/users/login");
+      });
+      
+  });
+});
+
+module.exports = router;
