5765d7e0ee9fc32667680f3a5ebe0497cb7b1204
As581 <av0232016@gmail.com>
2024-11-30 16:37:00 +0530

    Update mongoose-connection.js

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,1 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://av0232016:6CUKKHITgEOatspJ@blink.jacpt.mongodb.net/?retryWrites=true&w=majority&appName=Blink', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    process.exit(1);
+    console.error("MongoDB connection error:", error.message);
+  }
+};
+
+module.exports = connectDB;
 
