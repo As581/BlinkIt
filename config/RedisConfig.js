67845fa611d5003b61a1f044e78ab93dfc71d59e
As581 <av0232016@gmail.com>
2024-11-30 16:41:48 +0530

    Create RedisConfig.js

---
+++ config/RedisConfig.js
@@ -0,0 +1,19 @@
+const { createClient } = require('redis');
+
+const client = createClient({
+    password: 'FHBvbdLKW7hHHW8rdg1g27cNGiK7pdDb',
+    socket: {
+        host: 'redis-12557.c301.ap-south-1-1.ec2.redns.redis-cloud.com',
+        port: 12557,
+        connectTimeout: 10000 // 10 seconds timeout
+    }
+});
+
+// Connect to Redis with callback
+client.connect()
+    .then(() => console.log("Connected to Redis"))
+    .catch((err) => console.error("Redis connection error:", err));
+    
+
+
+module.exports = client;
