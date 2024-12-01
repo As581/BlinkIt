baba5c63b62830db7a7ef6c52683b161317ba072
SingleThreaded400 <av0232016@gmail.com>
2024-12-01 16:19:03 +0530

    Update mongoose-connection.js

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://av0232016:6CUKKHITgEOatspJ@blink.jacpt.mongodb.net/?retryWrites=true&w=majority&appName=Blink', {
+    await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
