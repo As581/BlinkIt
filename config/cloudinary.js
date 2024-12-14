597086592423770ec8cd649ee2fc288d97afd001
SingleThreaded400 <av0232016@gmail.com>
2024-12-14 16:53:06 +0530

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,8 +1,8 @@
 const cloudinary = require('cloudinary').v2;
 cloudinary.config({
-  cloud_name:'dgwqgrbof',
-  api_key:'457492125556172',
-  api_secret:'eBEzvKQWTSuFFxrhnb4N6ChzDG8',
+  cloud_name:process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret:process.env.API_SECRET,
 });
 
 module.exports = cloudinary;
