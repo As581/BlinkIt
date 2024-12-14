20486bd0a82c8d5d73c216ead726f8b0c9fb1407
SingleThreaded400 <av0232016@gmail.com>
2024-12-14 16:49:06 +0530

    Add files via upload

---
+++ config/cloudinary.js
@@ -0,0 +1,8 @@
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name:'dgwqgrbof',
+  api_key:'457492125556172',
+  api_secret:'eBEzvKQWTSuFFxrhnb4N6ChzDG8',
+});
+
+module.exports = cloudinary;
