25720aeaa687e2945d14179e03b3392b4b38e313
SingleThreaded400 <av0232016@gmail.com>
2025-07-17 17:21:54 +0530

    Update RedisConfig.js

--- config/RedisConfig.js
+++ config/RedisConfig.js
@@ -4,7 +4,7 @@ const client = createClient({
     password: process.env.REDIS_PASSWORD,
     socket: {
         host: 'redis-13221.c212.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 12557,
+        port: 13221,
         connectTimeout: 10000 // 10 seconds timeout
     }
 });
